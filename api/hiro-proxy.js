16025a74dbe057c85badff9f993113e89694a793
Flower <80220235+flormpecasique@users.noreply.github.com>
2025-03-11 21:01:21 +0100

    Update hiro-proxy.js api vercel

--- api/hiro-proxy.js
+++ api/hiro-proxy.js
@@ -21,7 +21,18 @@ export default async function handler(req, res) {
         }
 
         const data = await response.json();
-        res.status(200).json(data);
+
+        // Aquí puedes filtrar solo la información que te interesa
+        const result = {
+            address: data.address,
+            blockchain: data.blockchain,
+            status: data.status,
+            last_txid: data.last_txid,
+            zonefile_hash: data.zonefile_hash,
+            zonefile: data.zonefile
+        };
+
+        res.status(200).json(result);
     } catch (error) {
         console.error("Error fetching data from Hiro API:", error);
         res.status(500).json({ error: "Internal Server Error" });
